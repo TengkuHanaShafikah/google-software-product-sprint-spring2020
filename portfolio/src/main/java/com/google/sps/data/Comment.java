4268005b062669ef158ce5c5f0faeb1cf29ed981
Tengku Hana Shafikah <tghanashafikah@gmail.com>
2020-03-05 03:40:37 -0500

    Modified Comment class to have new attribute for the sentiment score.

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -7,12 +7,14 @@ public final class Comment {
   private final long id;
   private final String text;
   private final String name;
+  private final float score;
   private final Date date;
 
-  public Comment(long id, String name, String text, long time) {
+  public Comment(long id, String text, String name, float score, long time) {
     this.id = id;
     this.name = name;
     this.text = text;
+    this.score = score;
     this.date = new Date(time);
   }
 }
