4e33dd25bacd4f0e6d4b9b7bcb4698408f37ce68
Tengku Hana Shafikah <tghanashafikah@gmail.com>
2020-03-10 01:53:17 -0400

    Changed sentiment numerical score system to emoji vibe check system.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -37,26 +37,49 @@ function getComments() {
     });
 }
 
-/** Creates an <ul> element containing comment data.*/
+/**
+ * Returns HTML smiley emoji based on the sentiment score.
+ */
+ function returnEmoji(score) {
+  if (1 >= score && score > 0.6) { 
+    return `Vibe Check: ${String.fromCodePoint(0x1F600)}\n`; // Very happy
+  }
+  else if (0.6 >= score && score > 0.2) {
+    return `Vibe Check: ${String.fromCodePoint(0x1F642)}\n`; // Happy
+  } 
+  else if (0.2 >= score && score >= -0.2) {
+    return `Vibe Check: ${String.fromCodePoint(0x1F610)}\n`; // Neutral
+  } 
+  else if (-0.2 > score && score >= -0.6) {
+    return `Vibe Check: ${String.fromCodePoint(0x1F641)}\n`; // Upset
+  }
+  else {
+    return `Vibe Check: ${String.fromCodePoint(0x1F614)}\n`; // Very Upset
+  } 
+ }
+
+/**
+ * Creates a list element containing comment data.
+ */
 function createListElement(comment) {
-  const commentElement = document.createElement('ul');
+  const commentElement = document.createElement('li');
   commentElement.className = 'comment';
 
   const nameElement = document.createElement('b');
   nameElement.innerText = `${comment.name}: `;
 
   const textElement = document.createElement('span');
-  textElement.innerText = comment.text;
+  textElement.innerText = `${comment.text} \n`;
 
-  const scoreElement = document.createElement('span');
-  scoreElement.innerText = ` (Score: ${comment.score} |`;
-
+  const emojiElement = document.createElement('span');
+  emojiElement.innerText = returnEmoji(comment.score);
+  
   const timeElement = document.createElement('span');
-  timeElement.innerText = ` Date: ${comment.date})`;
+  timeElement.innerText = `Date: ${comment.date}`;
   
   commentElement.appendChild(nameElement);
   commentElement.appendChild(textElement);
-  commentElement.appendChild(scoreElement);
+  commentElement.appendChild(emojiElement);
   commentElement.appendChild(timeElement);
 
   return commentElement;
