a9c33161fc7eb33c8b978f055135476715020aef
Tengku Hana Shafikah <tghanashafikah@gmail.com>
2020-03-11 14:27:41 -0400

    Fixed styling of nonempty blocks.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -33,50 +33,48 @@ function getComments() {
     .then(comments => {
       const commentsListElement = document.getElementById("comments-list");
       commentsListElement.innerHTML = "";
-      comments.map(createListElement).forEach(line => commentsListElement.appendChild(line));
+      comments
+        .map(createListElement)
+        .forEach(line => commentsListElement.appendChild(line));
     });
 }
 
 /**
  * Returns HTML smiley emoji based on the sentiment score.
  */
- function returnEmoji(score) {
-  if (1 >= score && score > 0.6) { 
-    return `Vibe Check: ${String.fromCodePoint(0x1F600)}\n`; // Very happy
+function returnEmoji(score) {
+  if (1 >= score && score > 0.6) {
+    return `Vibe Check: ${String.fromCodePoint(0x1f600)}\n`; // Very happy
+  } else if (0.6 >= score && score > 0.2) {
+    return `Vibe Check: ${String.fromCodePoint(0x1f642)}\n`; // Happy
+  } else if (0.2 >= score && score >= -0.2) {
+    return `Vibe Check: ${String.fromCodePoint(0x1f610)}\n`; // Neutral
+  } else if (-0.2 > score && score >= -0.6) {
+    return `Vibe Check: ${String.fromCodePoint(0x1f641)}\n`; // Upset
+  } else {
+    return `Vibe Check: ${String.fromCodePoint(0x1f614)}\n`; // Very Upset
   }
-  else if (0.6 >= score && score > 0.2) {
-    return `Vibe Check: ${String.fromCodePoint(0x1F642)}\n`; // Happy
-  } 
-  else if (0.2 >= score && score >= -0.2) {
-    return `Vibe Check: ${String.fromCodePoint(0x1F610)}\n`; // Neutral
-  } 
-  else if (-0.2 > score && score >= -0.6) {
-    return `Vibe Check: ${String.fromCodePoint(0x1F641)}\n`; // Upset
-  }
-  else {
-    return `Vibe Check: ${String.fromCodePoint(0x1F614)}\n`; // Very Upset
-  } 
- }
+}
 
 /**
  * Creates a list element containing comment data.
  */
 function createListElement(comment) {
-  const commentElement = document.createElement('li');
-  commentElement.className = 'comment';
+  const commentElement = document.createElement("li");
+  commentElement.className = "comment";
 
-  const nameElement = document.createElement('b');
+  const nameElement = document.createElement("b");
   nameElement.innerText = `${comment.name}: `;
 
-  const textElement = document.createElement('span');
+  const textElement = document.createElement("span");
   textElement.innerText = `${comment.text} \n`;
 
-  const emojiElement = document.createElement('span');
+  const emojiElement = document.createElement("span");
   emojiElement.innerText = returnEmoji(comment.score);
-  
-  const timeElement = document.createElement('span');
+
+  const timeElement = document.createElement("span");
   timeElement.innerText = `Date: ${comment.date}`;
-  
+
   commentElement.appendChild(nameElement);
   commentElement.appendChild(textElement);
   commentElement.appendChild(emojiElement);
